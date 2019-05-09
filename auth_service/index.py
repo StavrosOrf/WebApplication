824668773ba928689f394a9aae5c9c0fbd5862fe
stavrosgreece <stayros_1@yahoo.gr>
2019-05-09 13:55:29 +0300

    Fixed Responses headers

--- auth_service/index.py
+++ auth_service/index.py
@@ -50,12 +50,6 @@ app.app.config['MONGO_URI'] = os.environ.get('DB')
 print("Connected to DEBUG mongodb")
 mongo = PyMongo(app.app)
 
-# @jwt.unauthorized_loader
-# def unauthorized_response(callback):
-#     return jsonify({
-#         'ok': False,
-#         'message': 'Missing Authorization Header'
-#     }), 401
 
 def decode_token(token):
     ''' Work-around to x-bearerInfoFunction required by connexion '''
@@ -78,111 +72,75 @@ def login():
         mongo.db.logged_in_users.insert_one({"email": email, "token": token})
 
         # print(decode(token,os.environ.get('SECRET'),algorithms=['HS256']))
-        resp = make_response.Response = jsonify({'token': token})
+        resp = jsonify({'token': token})
         resp.headers['Access-Control-Allow-Origin'] = '*'
         return resp, 200
-    return "Login failed",400
+    resp  = jsonify({'message': "Log in Failed"})
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp, 400    
 
 
 def logout(my_email):
     ''' logout user endpoint '''
     token = request.headers['Authorization'].split()[1]
-    db_response = mongo.db.logged_in_users.delete_one({'email': my_email, 'token': token})
+    db_response = mongo.db.logged_in_users.deleteMany({'email': my_email, 'token': token})
     if db_response.deleted_count == 1:
-        return "Successfully logged out", 200
+        resp = jsonify({'message': "Successfully logged out"})
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp, 200
+    resp = jsonify({'message': "Log out Failed"})
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp, 400    
 
 #@app.app.route('/register', methods=['POST'])
 def register():
     ''' register user endpoint '''
-    #data = validate_user(request.get_json())
-    #if data['ok']:
+
     req_body = request.get_json() 
     name, email, password  = req_body['name'], req_body['email'], req_body['password']
     password_hash = flask_bcrypt.generate_password_hash(password)
     # Insert registered user
-    #Check if reg users already exists
+    if mongo.db.reg_users.find_one({'email': email}):
+        resp = jsonify({'message': "Failed to  registered user:Email already in use!!"})
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp, 400
     mongo.db.reg_users.insert_one({"name": name, "email": email, "password": password_hash})
 
     URL = "http://app_logic_service:4010/api/users/add"
     data = {"email":email,"name": name}
     headers = {'Content-Type':'application/json'}
-    print(data)
+
     r = requests.post(URL,json = data) 
-    print(r.status_code)
-    print(r.reason)
+
     if r.status_code!=200 :
-        return "Failed to  registered user", 400
-    return "Successfully registered user", 200
+        resp = jsonify({'message': "Failed to  registered user"})
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp, 400
+
+    resp = jsonify({'message': "Successfully registered user"})
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp, 200  
 
 def authenticate(my_email):
     ''' auth request endpoint '''
-    #data = validate_user(request.get_json())
-    #if data['ok']:
+
     print(request.headers)
     # Get token from "Authorization: Bearer <token>" part of header
     token = request.headers['Authorization'].split()[1]
     logged_in_user = mongo.db.logged_in_users.find_one({'email': my_email, 'token': token})
     if logged_in_user:
-        return "Successfully authenticated user", 200
-    return "failed to authenticate user", 400
+        resp = jsonify({'message': "Successfully authenticated user"})
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp, 200  
+    resp = jsonify({'message':"Failed to authenticate user"})
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp, 400      
     
-# @app.app.route('/refresh', methods=['POST'])
-# @jwt_refresh_token_required
-# def refresh():
-#     ''' refresh token endpoint '''
-#     current_user = get_jwt_identity()
-#     ret = {
-#         'token': create_access_token(identity=current_user)
-#     }
-#     return jsonify({'ok': True, 'data': ret}), 200
 
-# @app.app.route('/user', methods=['GET', 'DELETE', 'PATCH'])
-# @jwt_required
-# def user():
-#     ''' route read user '''
-#     if request.method == 'GET':
-#         query = request.args
-#         data = mongo.db.users.find_one(query, {"_id": 0})
-#         return jsonify({'ok': True, 'data': data}), 200
-
-#     data = request.json()
-#     if request.method == 'DELETE':
-#         if data.get('email', None) is not None: #             db_response = mongo.db.users.delete_one({'email': data['email']}) #             if db_response.deleted_count == 1:
-#                 response = {'ok': True, 'message': 'record deleted'}
-#             else:
-#                 response = {'ok': True, 'message': 'no record found'}
-#             return jsonify(response), 200
-#         else:
-#             return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
-
-#     if request.method == 'PATCH':
-#         if data.get('query', {}) != {}:
-#             mongo.db.users.update_one(data['query'], {'$set': data.get('payload', {})})
-#             return jsonify({'ok': True, 'message': 'record updated'}), 200
-#         else:
-#             return jsonify({'ok': False, 'message': 'Bad request parameters!'}), 400
-
-# @app.errorhandler(404)
-# def not_found(error):
-#     """ error handler """
-#     #LOG.error(error)
-#     return make_response(jsonify({'error': 'Not found'}), 404)
-
-
-# @app.route('/')
-# def index():
-#     """ static files serve """
-#     return send_from_directory('dist', 'index.html')
-
-
-# @app.route('/<path:path>')
-# def static_proxy(path):
-#     """ static folder serve """
-#     file_name = path.split('/')[-1]
-#     dir_name = os.path.join('dist', '/'.join(path.split('/')[:-1]))
-#     return send_from_directory(dir_name, file_name)
-
-
+    # if logged_in_user:
+    #     return "Successfully authenticated user", 200
+    # return "failed to authenticate user", 400
+    
 if __name__ == '__main__':
     #LOG.info('running environment: %s', os.environ.get('ENV'))
     app.run(os.environ.get('PORT'))
