64251c2e246a8c8a1a7c9ead9eda126a1e2eb787
ckarageorgkaneen <ckarageorgkaneen@gmail.com>
2019-05-10 19:18:58 +0300

    add more functions

--- web_service/app/UI/index.js
+++ web_service/app/UI/index.js
@@ -1,64 +1,3 @@
-var friends = get_friends();
-
-String.prototype.format = function() {
-  a = this;
-  for (k in arguments) {
-    a = a.replace("{" + k + "}", arguments[k])
-  }
-  return a
-}
-
-var profile_card_html = `<div class="card">
-  <img src="some_image.jpg" alt=" " style="width:100%">
-  <p class="title" style="color: grey;font-size: 30px;">{0}</p>
-	<a href={1}><i class="fa fa-envelope"></i></a> 
-	{2}
-  <p><button class="galleries-btn" onClick="render_galleries();">Galleries</button></p>
-</div>`;
-
-var profile_section_html = `<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-<style>
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  max-width: 200px;
-  margin: auto;
-  text-align: center;
-  font-family: arial;
-}
-
-.galleries-btn {
-  border: none;
-  outline: 0;
-  display: inline-block;
-  padding: 8px;
-  color: white;
-  background-color: #267fdd;
-  text-align: center;
-  cursor: pointer;
-  width: 100%;
-  font-size: 18px;
-}
-
-.fa {
-  text-decoration: none;
-  font-size: 22px;
-  color: #267fdd;
-}
-
-.galleries-btn:hover, .fa:hover {
-  opacity: 0.7;
-}
-</style>
-</head>
-<body>
-
-{0}
-</body>
-</html>`;
-
 function get_my_email(){
 	my_email = "ckarageorgkaneen@gmail.com";//sessionStorage.getItem('my_email');
 	return my_email;
@@ -73,37 +12,6 @@ function get_user_name(email){
 function get_user(email){
 	name = get_user_name(email);
 	return {name: name, email: email};
-}
-
-function make_profile_card_html(user){
-	var is_user_added = true;
-	var name = user.name;
-	var email = user.email;
-	var mail_to_email = "mailto:" + email;
-	var friend_btn_html = is_user_added?
-		'<a href="#"><i class="fa fa-user-minus"></i></a>':
-		'<a href="#"><i class="fa fa-user-plus"></i></a>';
-	return profile_card_html.format(name, mail_to_email, friend_btn_html)
-}
-
-function render_profile_UI(user){
-	fd_profile_card_html = make_profile_card_html(user);
-	return profile_section_html.format(fd_profile_card_html);
-}
-
-function get_galleries(){
-	// Ajax call
-	return "";
-}
-
-function make_pretty_galleries_UI(galleries){
-	return "Galleries";
-}
-
-function render_galleries(){
-	var galleries = get_galleries();
-  	var galleries_html = make_pretty_galleries_UI(galleries);
-	$("#content").html(galleries_html)
 }
 
 function get_friends(){
@@ -128,13 +36,163 @@ function get_friends(){
 	return friends;
 }
 
-function render_friends_UI(friends){
-	friend_profiles_html = '';
-	friends.forEach(function (friend) {
-    	friend_profiles_html += make_profile_card_html(friend);
-	});
+function get_galleries(email){
+	// Ajax call
+	images = [
+		{
+			name: "Cats"
+		},
+		{
+			name: "Dogs"
+		},
+		{
+			name: "Turtles"
+		}
+	];
 
-	return profile_section_html.format(friend_profiles_html);
+	return images;
+}
+
+var my_email = get_my_email();	
+var my_user = get_user(my_email);
+var my_friends = get_friends();
+var my_galleries = get_galleries();
+
+String.prototype.format = function() {
+  a = this;
+  for (k in arguments) {
+    a = a.replace("{" + k + "}", arguments[k])
+  }
+  return a
+}
+
+var profile_card_html = `<div class="card">
+  <img src="some_image.jpg" alt=" " style="width:100%">
+  <p class="title" style="color: grey;font-size: 30px;">{0}</p>
+	<a href={1}><i class="fa fa-envelope"></i></a> 
+	{2}
+  <p><button class="galleries-btn" onClick={3}>Galleries</button></p>
+</div>`;
+
+var gallery_html = `<div class="card">
+  <img src="some_image.jpg" alt=" " style="width:100%">
+  <div class="title" style="color: grey;font-size: 30px;">{0} <i class="fa fa-images"></i></div>	
+  <p><button class="view_gallery_images-btn" onClick={1}>View</button></p>
+</div>`;
+   
+
+var content_section_html = `<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+<style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 200px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+}
+
+.galleries-btn, .view_gallery_images-btn {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #267fdd;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+.fa {
+  text-decoration: none;
+  font-size: 22px;
+  color: #267fdd;
+}
+
+.galleries-btn:hover, .fa:hover {
+  opacity: 0.7;
+}
+</style>
+</head>
+<body>
+{0}
+</body>
+</html>`;
+
+function make_profile_card_html(user){
+	var is_user_added = true;
+	var name = user.name;
+	var email = user.email;
+	var mail_to_email_str = "mailto:" + email;
+	var friend_btn_html = is_user_added?
+		'<a href="#"><i class="fa fa-user-minus"></i></a>':
+		'<a href="#"><i class="fa fa-user-plus"></i></a>';
+	var render_glrs_UI_func_str = "render_galleries_UI(" + email + ");"
+	return profile_card_html.format(name, 
+									mail_to_email_str, 
+									friend_btn_html, 
+									render_glrs_UI_func_str);
+}
+
+function render_my_profile_UI(){
+	var my_profile_card_html = make_profile_card_html(my_user);
+	return content_section_html.format(my_profile_card_html);
+}
+
+function make_image_html(image){
+	var img_name = image.name;
+	var img_link = image.link;
+}
+
+function make_gallery_images_UI(images){
+	var glr_images_html = '';
+	images.forEach(function(image){
+		glr_images_html += make_image_html(image);
+	})
+	return glr_images_html;
+}
+
+function render_gallery_images_UI(glr_name, email){
+	var images = get_images(glr_name, email);
+	var gallery_images_html = make_gallery_images_UI(images);
+	return content_section_html.format(gallery_images_html);
+}
+
+function make_gallery_html(gallery, email){
+	var glr_name = gallery.name;
+	var view_glr_imgs_func_str = "render_gallery_images_UI("+glr_name+","+email+");";
+	return gallery_html.format(glr_name, view_glr_imgs_func_str);
+}
+
+function make_galleries_UI(galleries, email){
+	var galleries_html = '';
+	galleries.forEach(function(gallery){
+		galleries_html += make_gallery_html(gallery, email);
+	});
+	return galleries_html;
+}
+
+function render_galleries_UI(email){
+	var galleries = get_galleries(email);
+  	var galleries_html = make_galleries_UI(galleries, email);
+	return content_section_html.format(galleries_html);
+}
+
+function make_my_friends_UI(){
+	var my_friends_html = '';
+	my_friends.forEach(function(friend){
+    	my_friends_html += make_profile_card_html(friend);
+	});
+	return my_friends_html;
+}
+
+function render_my_friends_UI(){
+	var my_friends_html = make_my_friends_UI();
+	return content_section_html.format(my_friends_html);
 }
 
 $(document).ready(function(){
@@ -143,19 +201,18 @@ $(document).ready(function(){
 })
 
 $(".profile-button").click(function(){
-	var my_email = get_my_email();
-	var my_user = get_user(my_email);
-  	var my_profile_UI_html = render_profile_UI(my_user);
+	var my_profile_UI_html = render_my_profile_UI();
 	$("#content").html(my_profile_UI_html);
 })
   
 $(".galleries-button").click(function(){
-	render_galleries();
+  	var my_galleries_UI_html = render_galleries_UI(my_email);
+	$("#content").html(my_galleries_UI_html);
 })
 
 $(".friends-button").click(function(){ 
-	var friends_UI_html = render_friends_UI(friends);
-	$("#content").html(friends_UI_html);
+	var my_friends_UI_html = render_my_friends_UI();
+	$("#content").html(my_friends_UI_html);
 })
   
 })
